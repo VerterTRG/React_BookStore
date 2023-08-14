3862c46a24b6e308b978794af9c08648a3d48c21
Aleksei Ignatev <vertertrg@gmail.com>
2023-08-13 17:05:11 -0700

    Not found page implementation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import BookDetail from "../BooksList/BookDetail/BookDetail";
 import BookList from "../BooksList/BookList";
+import NotFound from "../NotFound/NotFound";
 import SearchBar from "../SearchBar/SearchBar";
 import { useState } from "react";
 
@@ -17,16 +18,11 @@ function App() {
         <div className="container" style={{ margin: 50 }}>
           <SearchBar setSearch={setSearch} />
           <Routes>
-            <Route
-              path="/"
-              element={
-                <BookList search={search} />
-              }
-            />
-            <Route
-              path="/book/:bookId"
-              element={<BookDetail />}
-            />
+            <Route path="/" element={<BookList search={search} />} />
+            <Route path="/book/:bookId" element={<BookDetail />} />
+            
+            <Route path="*" element={<Navigate to="/404" />} />
+            <Route path="/404" element={<NotFound />} />
           </Routes>
 
           {/* {selectedBook && <BookDetail />} */}
